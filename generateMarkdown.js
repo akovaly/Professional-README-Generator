61630059c3cc7bc30f35ba18fdef6c47967eeedd
akovaly <adamjkovaly@gmail.com>
2024-10-23 21:56:36 -0600

    updated generateMarkdown.js, index.js, package-lock.json, package.json

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,19 +1,115 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
+function renderLicenseBadge(license) {
+    if (license === 'MIT') {
+        return '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)';
+    } else if (license === 'GNU GPLv3') {
+        return '![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)';
+    } else if (license === 'Apache 2.0') {
+        return '![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)';
+    } else if (license === 'ISC') {
+        return '![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)';
+    } else {
+        return '';
+    }
+}
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+    if (license === 'MIT') {
+        return '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)';
+    } else if (license === 'GNU GPLv3') {
+        return '![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)';
+    } else if (license === 'Apache 2.0') {
+        return '![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)';
+    } else if (license === 'ISC') {
+        return '![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)';
+    } else {
+        return '';
+    }
+}
 
 // TODO: Create a function that returns the license link
+function renderLicenseLink(license) {
+    if (license === 'MIT') {
+        return '[MIT](https://opensource.org/licenses/MIT)';
+    } else if (license === 'GNU GPLv3') {
+        return '[GNU GPLv3](https://www.gnu.org/licenses/gpl-3.0)';
+    } else if (license === 'Apache 2.0') {
+        return '[Apache 2.0](https://opensource.org/licenses/Apache-2.0)';
+    } else if (license === 'ISC') {
+        return '[ISC](https://opensource.org/licenses/ISC)';
+    } else {
+        return '';
+    }
+}
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+    if (license === 'MIT') {
+        return '[MIT](https://opensource.org/licenses/MIT)';
+    } else if (license === 'GNU GPLv3') {
+        return '[GNU GPLv3](https://www.gnu.org/licenses/gpl-3.0)';
+    } else if (license === 'Apache 2.0') {
+        return '[Apache 2.0](https://opensource.org/licenses/Apache-2.0)';
+    } else if (license === 'ISC') {
+        return '[ISC](https://opensource.org/licenses/ISC)';
+    } else {
+        return '';
+    }
+}
 
 // TODO: Create a function that returns the license section of README
+function renderLicenseSection(license) { 
+  if (license === 'MIT') {
+      return 'This project is licensed under the MIT license.';
+  }
+}
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license === 'MIT') {
+      return 'This project is licensed under the MIT license.';
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+${renderLicenseBadge(data.license)}
 
+## Description
+${data.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contribution](#contribution)
+- [Test](#test)
+- [License](#license)
+- [Questions](#questions)
+- [Contact](#contact)
+- [License](#license)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## Contribution
+${data.contribution}
+
+## Test
+${data.test}
+
+## License
+${renderLicenseSection(data.license)}
+${renderLicenseLink(data.license)}
+
+## Questions
+If you have any questions, please reach out to me at ${data.email}.
+You can also visit my GitHub profile at [${data.github}](
+
+## Contact
+If you have any questions, please reach out to me at ${data.email}.
+You can also visit my GitHub profile at [${data.github}](
 `;
 }
 
--- index.js
+++ index.js
@@ -1,13 +1,82 @@
 // TODO: Include packages needed for this application
+import inquirer from 'inquirer';
+import fs from 'fs';
+import generateMarkdown from './utils/generateMarkdown';
+import { type } from 'os';
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please provide a description of your project.'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Please provide installation instructions.'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please provide usage information.'
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'Please provide contribution guidelines.'
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Please provide test instructions.'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Please select a license.',
+        choices: ['MIT', 'GNU GPLv3', 'Apache 2.0', 'ISC', 'None']
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Please provide your GitHub username.'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Please provide your email address.'
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log('README.md file successfully created!');
+        }
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+        .then((answers) => {
+            writeToFile('README.md', generateMarkdown(answers));
+        });
+}
 
 // Function call to initialize app
-init();
+function init() {
+    inquirer.prompt(questions)
+        .then((answers) => {
+            writeToFile('README.md', generateMarkdown(answers));
+        });
+}
